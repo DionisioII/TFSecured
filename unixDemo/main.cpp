4c64f80f71228bae271ca6be86231bb4460a4c20
Alex iOS <ovechko.056@gmail.com>
2019-03-07 21:30:55 +0200

    Added tensorflow

--- unixDemo/main.cpp
+++ unixDemo/main.cpp
@@ -8,11 +8,13 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <tensorflow/c/c_api.h>
+
 
 using namespace cv;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World   !\n";
+    std::cout << "TF version = " << TF_Version() << std::endl;
     return 0;
 }
